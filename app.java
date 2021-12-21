078857141549a4d72bc70ba03b6e625ab16a6551
davidfortunato <76709704+davidfortunato@users.noreply.github.com>
2021-12-21 16:22:22 +0000

    Add files via upload

--- app.java
+++ app.java
@@ -20,6 +20,8 @@ public class app {
         Pathway pathway4 = new Pathway ("p4");
         Pathway pathway5 = new Pathway ("p5");
         Pathway pathway6 = new Pathway ("p6");
+        Pathway pathway7 = new Pathway ("p7");
+        Pathway pathway8 = new Pathway ("p8");
 
         Room cell_blockA = new Room ("CBA");
         Room cell_blockB = new Room ("CBB");
@@ -34,6 +36,7 @@ public class app {
         Room  kitchen = new Room ("KIT");
         Room  laundry_room = new Room ("LR");
         Room  infermary = new Room ("SOS");
+        Room  visitationArea = new Room ("VA");
 
         prison.addSpace(hallway1);
         prison.addSpace(hallway2);
@@ -47,7 +50,9 @@ public class app {
         prison.addSpace(pathway3);
         prison.addSpace(pathway4);
         prison.addSpace(pathway5);
-        prison.addSpace(pathway6);
+        prison.addSpace(pathway6);  
+        prison.addSpace(pathway7);
+        prison.addSpace(pathway8);
 
         prison.addSpace(cell_blockA);
         prison.addSpace(cell_blockB);
@@ -62,108 +67,115 @@ public class app {
         prison.addSpace(kitchen);
         prison.addSpace(laundry_room);
         prison.addSpace(infermary);
+        prison.addSpace(visitationArea);
 
         //adjacents para o hallway 1
         Adjacent a1 = new Adjacent(cell_blockA, 6);
         Adjacent a2 = new Adjacent (cafeteria, 1);
-        Adjacent a3 = new Adjacent (pathway1, 4);
-        Adjacent a4 = new Adjacent (pathway2, 4);
-        Adjacent a5 = new Adjacent (kitchen, 6);
-        Adjacent a6 = new Adjacent (showers, 1);
-        Adjacent a7 = new Adjacent (pathway3, 1);
-        Adjacent a8 = new Adjacent (cell_blockB, 1);
-        Adjacent a9 = new Adjacent (pathway4, 1);
-        Adjacent a10 = new Adjacent (laundry_room, 1);
-        Adjacent a11 = new Adjacent (hallway2, 1);
+        Adjacent a3 = new Adjacent (cell_blockB, 1);
 
-        hallway1.addAdjacentList(Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11));
+        hallway1.addAdjacentList(Arrays.asList(a1, a2, a3));
         
         // adjacents in hallway2
-        Adjacent b1 = new Adjacent(laundry_room, 1);
-        Adjacent b2 = new Adjacent (football_field, 1);
-        Adjacent b3 = new Adjacent (pathway5, 1);
+        Adjacent b1 = new Adjacent(cafeteria, 1);
+        Adjacent b2 = new Adjacent (maintenanceArea, 1);
+        Adjacent b3 = new Adjacent (kitchen, 1);
 
         hallway2.addAdjacentList(Arrays.asList(b1, b2, b3));
 
         // adjacents in hallway3
-        Adjacent c1 = new Adjacent(cell_blockC, 1);
-        Adjacent c2 = new Adjacent (pathway6, 1);
-        Adjacent c3 = new Adjacent (pathway5, 1); 
-        Adjacent c4 = new Adjacent (laundry_room, 1); 
-        Adjacent c5 = new Adjacent (recess, 1);
-        Adjacent c6 = new Adjacent (showers, 1);  
-        Adjacent c7 = new Adjacent (pathway3, 1);  
-        Adjacent c8 = new Adjacent (cell_blockB, 1);
-        Adjacent c9 = new Adjacent (pathway4, 1); 
+        Adjacent c1 = new Adjacent(kitchen, 1);
+        Adjacent c2 = new Adjacent (solitary, 1);
+        Adjacent c3 = new Adjacent (visitationArea, 1); 
 
-        hallway3.addAdjacentList(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9));
+        hallway3.addAdjacentList(Arrays.asList(c1, c2, c3));
 
         //adjacents in hallway4
 
-        Adjacent d1 = new Adjacent(football_field, 1);
-        Adjacent d2 = new Adjacent (pathway5, 1);
-        Adjacent d3 = new Adjacent (basketball_court, 1);
+        Adjacent d1 = new Adjacent(cell_blockB, 1);
+        Adjacent d2 = new Adjacent (showers, 1);
+        Adjacent d3 = new Adjacent (cell_blockC, 1);
+        Adjacent d4 = new Adjacent (recess, 1);
 
-        hallway4.addAdjacentList(Arrays.asList(d1, d2, d3));
+        hallway4.addAdjacentList(Arrays.asList(d1, d2, d3, d4));
 
         // adjacents in hallway5   
-        Adjacent e1 = new Adjacent(cell_blockC, 1);
-        Adjacent e2 = new Adjacent (pathway6, 1);
-        Adjacent e3 = new Adjacent (maintenanceArea, 1);
+        Adjacent e1 = new Adjacent(maintenanceArea, 1);
+        Adjacent e2 = new Adjacent (cell_blockB, 1);
+        Adjacent e3 = new Adjacent (laundry_room, 1);
+        Adjacent e4 = new Adjacent (infermary, 1);
 
-        hallway5.addAdjacentList(Arrays.asList(e1, e2, e3));
+        hallway5.addAdjacentList(Arrays.asList(e1, e2, e3, e4));
 
         // adjacents in hallway6
-        Adjacent l1 = new Adjacent(basketball_court, 1);
-        Adjacent l2 = new Adjacent (pathway5, 1);
-        Adjacent l3 = new Adjacent (infermary, 1);
+        Adjacent l1 = new Adjacent(solitary, 1);
+        Adjacent l2 = new Adjacent (laundry_room, 1);
+        Adjacent l3 = new Adjacent (football_field, 1);
+        Adjacent l4 = new Adjacent (basketball_court, 1);
 
-        hallway5.addAdjacentList(Arrays.asList(l1, l2, l3));
+        hallway5.addAdjacentList(Arrays.asList(l1, l2, l3, l4));
 
         // adjacents in pathway1
 
         Adjacent f1 = new Adjacent(cell_blockA, 1);
-        Adjacent f2 = new Adjacent (cafeteria, 1);
+        Adjacent f2 = new Adjacent (cell_blockC, 1);
+        Adjacent f3 = new Adjacent (showers, 1);
 
-        pathway1.addAdjacentList(Arrays.asList(f1, f2));
+        pathway1.addAdjacentList(Arrays.asList(f1, f2, f3));
 
         // adjacents in pathway2
 
-        Adjacent g1 = new Adjacent(kitchen, 1);
-        Adjacent g2 = new Adjacent (cafeteria, 1);
+        Adjacent g1 = new Adjacent(cafeteria, 1);
+        Adjacent g2 = new Adjacent (maintenanceArea, 1);
+        Adjacent g3 = new Adjacent (cell_blockC, 1);
+        Adjacent g4 = new Adjacent (cell_blockB, 1);
 
-        pathway2.addAdjacentList(Arrays.asList(g1, g2));
+        pathway2.addAdjacentList(Arrays.asList(g1, g2, g3, g4));
 
         // adjacents in pathway3
 
-        Adjacent h1 = new Adjacent(showers, 1);
-        Adjacent h2 = new Adjacent (cell_blockB, 1);
+        Adjacent h1 = new Adjacent(kitchen, 1);
+        Adjacent h2 = new Adjacent (maintenanceArea, 1);
+        Adjacent h3 = new Adjacent (solitary, 1);
+        Adjacent h4 = new Adjacent (laundry_room, 1);
 
-        pathway3.addAdjacentList(Arrays.asList(h1, h2));
+        pathway3.addAdjacentList(Arrays.asList(h1, h2, h3, h4));
 
         //adjacents in pathway4
 
-        Adjacent i1 = new Adjacent(laundry_room, 1);
-        Adjacent i2 = new Adjacent (cell_blockB, 1);
+        Adjacent i1 = new Adjacent(visitationArea, 1);
+        Adjacent i2 = new Adjacent (solitary, 1);
+        Adjacent i3 = new Adjacent (football_field, 1);
 
-        pathway4.addAdjacentList(Arrays.asList(i1, i2));
+        pathway4.addAdjacentList(Arrays.asList(i1, i2, i3));
 
         // adjacents in pathway5
 
-        Adjacent j1 = new Adjacent(laundry_room, 1);
-        Adjacent j2 = new Adjacent (football_field, 1);
-        Adjacent j3 = new Adjacent(basketball_court, 1);
-        Adjacent j4 = new Adjacent (recess, 1);
+        Adjacent j1 = new Adjacent(recess, 1);
+        Adjacent j2 = new Adjacent (showers, 1);
 
-        pathway5.addAdjacentList(Arrays.asList(j1, j2, j3, j4));
+        pathway5.addAdjacentList(Arrays.asList(j1, j2));
 
         // adjacents in pathway6
-        Adjacent k1 = new Adjacent (cell_blockC, 1);
-        Adjacent k2 = new Adjacent(maintenanceArea, 1);
-        Adjacent k3 = new Adjacent (recess, 1);
+        Adjacent k1 = new Adjacent (recess, 1);
+        Adjacent k2 = new Adjacent(cell_blockB, 1);
+        Adjacent k3 = new Adjacent (infermary, 1);
 
         pathway6.addAdjacentList(Arrays.asList(k1, k2, k3));
+
+        // adjacents in pathway7
+        Adjacent m1 = new Adjacent (infermary, 1);
+        Adjacent m2 = new Adjacent(laundry_room, 1);
+        Adjacent m3 = new Adjacent (basketball_court, 1);
     
+        pathway6.addAdjacentList(Arrays.asList(l1, l2, l3));
+           
+        // adjacents in pathway8
+        Adjacent n1 = new Adjacent (football_field, 1);
+        Adjacent n2 = new Adjacent(basketball_court, 1);
+    
+        pathway6.addAdjacentList(Arrays.asList(k1, k2));
+
         return prison;
     }
 
